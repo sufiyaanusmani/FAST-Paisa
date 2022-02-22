b14e771ab74a0640fa9d73d6d6a8301659e107a6
Sufiyaan Usmani <k213195@nu.edu.pk>
2022-02-22 18:31:53 +0500

    added deleteAccount() for customer

--- ui.h
+++ ui.h
@@ -68,6 +68,52 @@ void currentDateAndTime()
     cout << ctime(&t);
 }
 
+int mainMenu()
+{
+    int choice, i;
+    system("color 0F");
+    system("cls");
+    system("title FAST NUCES BANK - MAIN MENU");
+    CursorPosition(0, 0);
+    TextColor(10);
+    currentDateAndTime();
+    CursorPosition(32, 3);
+    TextColor(11);
+    cout << "\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 FAST-NUCES BANK \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2";
+    for (i = 1; i <= 7; i++)
+    {
+        CursorPosition(31, 3 + i);
+        printf("|");
+    }
+    for (i = 1; i <= 7; i++)
+    {
+        CursorPosition(91, 3 + i);
+        printf("|");
+    }
+    for (i = 1; i <= 60; i++)
+    {
+        CursorPosition(31 + i, 11);
+        printf("-");
+    }
+    TextColor(15);
+    CursorPosition(33, 5);
+    cout << "1. Login as Admin";
+    CursorPosition(33, 6);
+    cout << "2. Login as Customer";
+    CursorPosition(33, 7);
+    cout << "3. Create new account";
+    CursorPosition(33, 8);
+    cout << "4. Check today's currency rates";
+    CursorPosition(33, 9);
+    cout << "5. About Us";
+    CursorPosition(33, 10);
+    cout << "6. Exit";
+    CursorPosition(32, 15);
+    cout << "Enter your choice: ";
+    fflush(stdin);
+    cin >> choice;
+    return choice;
+}
 
 
 #endif
